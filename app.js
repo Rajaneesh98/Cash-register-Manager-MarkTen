b03d674b4b0936b2a47670035a57691c2ff6bb3f
Rajaneesh <rajaneesh1998@gmail.com>
2022-09-05 17:42:40 +0530

    bug fix

--- app.js
+++ app.js
@@ -13,19 +13,21 @@ var denominationList = [2000, 500, 100, 20, 10, 5, 1];
 function checkForReturnChange() {
   var bill = Number(billAndCash[0].value);
   var cashGiven = Number(billAndCash[1].value);
-  //console.log(bill)
+  if ((bill > 0) & (cashGiven > 0)) {
+    if (cashGiven < bill) {
+      warningEl.innerText = "Go and Wash The Plates For sometime.";
+    } else {
+      warningEl.innerText = "";
+      let returnChange = cashGiven - bill;
 
-  if (cashGiven < bill) {
-    warningEl.innerText = "Go and Wash THe Plates For sometime";
-  } else {
-    warningEl.innerText = "";
-    let returnChange = cashGiven - bill;
-
-    for (var i = 0; i < denominationList.length; i++) {
-      var respectiveNotes = Math.trunc(returnChange / denominationList[i]);
-      returnChange = returnChange % denominationList[i];
-      rowEls[i].innerText = respectiveNotes;
+      for (var i = 0; i < denominationList.length; i++) {
+        var respectiveNotes = Math.trunc(returnChange / denominationList[i]);
+        returnChange = returnChange % denominationList[i];
+        rowEls[i].innerText = respectiveNotes;
+      }
     }
+  } else {
+    warningEl.innerText = "Enter Valid Values";
   }
 }
 
